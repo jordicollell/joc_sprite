eed66dbd9110ad218a4763b68c46d219ccfc518a
jordicollell <78138546+jordicollell@users.noreply.github.com>
2021-04-08 13:41:28 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,20 +4,24 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     jugador.change(LedSpriteProperty.X, 1)
 })
+let obstacle_2: game.LedSprite = null
 let obstacle: game.LedSprite = null
 let jugador: game.LedSprite = null
 jugador = game.createSprite(2, 4)
 basic.forever(function () {
     basic.pause(randint(4000, 6000))
     obstacle = game.createSprite(randint(0, 4), 0)
+    obstacle_2 = game.createSprite(randint(0, 4), 0)
     for (let index = 0; index < 4; index++) {
-        basic.pause(20)
+        basic.pause(100)
         obstacle.change(LedSpriteProperty.Y, 1)
+        obstacle_2.change(LedSpriteProperty.Y, 1)
     }
-    if (jugador.isTouching(obstacle)) {
+    if (jugador.isTouching(obstacle) || jugador.isTouching(obstacle_2)) {
         game.gameOver()
     }
     basic.pause(200)
     game.addScore(1)
     obstacle.delete()
+    obstacle_2.delete()
 })
